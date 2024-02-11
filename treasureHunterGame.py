539f7338eba30b66dafd945eac676329c1e925d0
mrorobot <109413727+mrorobot@users.noreply.github.com>
2024-02-11 22:36:02 +0530

    Add files via upload

---
+++ treasureHunterGame.py
@@ -0,0 +1,162 @@
+print(("you are living in an era where the living condition of life are poor the "
+       "world is divided into two major sectors the 5percent rich and  the 95 percent"
+       "poor people are live in a small community so roumers are spread easly"
+       "they have blind faith and they are irrationally imaginative"))
+
+print(("You are a fearless adventurer and renowned treasure hunter."
+       "Legend has it that the Lost Treasure of Atlantis, the treasure worth more than bilion"
+       "make you the richest man in the world, lies hidden deep within an ancient temple in a remote jungle."
+        "Determined to claim this treasure, you embark on an epic quest."))
+
+print(("As you enter the dense jungle, you come across a fork(two paths) in the path."
+"To the left is a treacherous swamp, rumored to be inhabited by dangerous creatures."
+"To the right is a dense thicket of thorns, concealing potential traps."))
+enter=input("So which path will you choose?? , 'Left' or 'Right'")
+enter.lower()
+if enter=="left":
+    print("congratulation the path is difficult , there is no creature its just a roumer")
+    print("Ahead, you spot an ancient stone door guarding the entrance to the temple.")
+    print(("you encounter the beautiful women with white face red lips and the perfect curved"
+           "body she has the white robe on her body"
+          "asking your help to help her child who has his leg trap in the stone"))
+    print("her boy recently joined the group of bandits" )
+    print("would you help her")
+    enter5=input("Help OR Ignore")
+    enter5.lower()
+    if enter5=="help":
+        print("you free her boy and impress the women")
+        print("now proceed to the temple")
+    elif enter5=="ignore":
+        print('now you can see the intrance of the tample')
+
+        print("""
+
+[][][] /""\ [][][]
+  |::| /____\ |::|
+  |[]|_|::::|_|[]|
+  |::::::__::::::|
+  |:::::/||\:::::|
+  |:#:::||||::#::|
+ #%*###&*##&*&#*&##
+##%%*####*%%%###*%*#
+""")
+
+
+
+
+
+if enter=="right":
+    print(("you encounter the beautiful women with white face red lips and the perfect curved"
+           "body she has the white robe on her body"
+          "asking your help to help her child who has his leg trap in the stone back "
+          "in the cave , in excahnge she will assist you in your way but if you not help her"
+          "she will not let you cross the path"))
+    enter1=input("what will be your choice?? help or fight")
+    if enter1=="help":
+        
+
+
+    
+        print("you help her boy now she is with you in your journy")
+    elif enter1=="fight":
+        print("she try to stop you by her life now you killed her")
+        print(("in your path you hear the voice of the boy asking for help in the cave "
+           "you have his mother blood in your cloth so he will recognize you and try to"
+           "take your life if you help him what will you do??"))
+        print("the cost of helping him is that you can loss atleast your one hand and then if you get the treasure you cant collect full")
+        enter2=input("help or ignore")
+
+        if enter2=="ignore":
+            print("congratulations you cross the path")
+        elif enter2=="help":
+            print("you cross the path but you have massive bleeding in your one hand")
+            print ("you can cut your hand or wait to find a medicine for it")
+            print("but it can be rotten and your whole body can be poison")
+            enter3=input("what you choose?? Cut or wait")
+            if enter3=="cut":
+                print("ohk you save for now")
+                print('now you can see the intrance of the tample')
+
+                print("""
+
+[][][] /""\ [][][]
+  |::| /____\ |::|
+  |[]|_|::::|_|[]|
+  |::::::__::::::|
+  |:::::/||\:::::|
+  |:#:::||||::#::|
+ #%*###&*##&*&#*&##
+##%%*####*%%%###*%*#
+""")
+
+                print(("Now you enters a dimly lit chamber with three gates,"
+                "each adorned with a different color: Red, Blue, and Green."
+                  "Above the gates, an inscription reads:"
+                  "Choose the correct gate to proceed and avoid a grisly fate."
+                  "The gate that lies beyond the color of blood shall guide your way."
+                  "The treasure hunter must select the gate that matches the clue"
+                 "and leads to the next section while avoiding potential traps behind the other gates."))
+                print("so what color of gate you choose")
+                enter4= input("Red , Yellow , Green")
+                enter4.lower
+                if enter4=="red":
+                    print("congratulation you got the treasure")
+                    print("""
+                            _.--.
+                        _.-'_:-'||
+                    _.-'_.-::::'||
+               _.-:'_.-::::::'  ||
+             .'`-.-:::::::'     ||
+            /.'`;|:::::::'      ||_
+           ||   ||::::::'     _.;._'-._
+           ||   ||:::::'  _.-!oo @.!-._'-.
+           \.  ||:::::.-!()oo @!()@.-'_.|
+            '.'-;|:.-'.&$@.& ()$%-'o.'\U||
+              `>'-.!@%()@'@_%-'_.-o _.|'||
+               ||-._'-.@.-'_.-' _.-o  |'||
+               ||=[ '-._.-\U/.-'    o |'||
+               || '-.]=|| |'|      o  |'||
+               ||      || |'|        _| ';
+               ||      || |'|    _.-'_.-'
+               |'-._   || |'|_.-'_.-'
+            jgs '-._'-.|| |' `_.-'
+                    '-.||_/.-'
+    """)
+else:             
+    print("the wrong selection of choice, there is dead end")
+    print("""
+             /)_(\
+                                           ______( 0 0 )______
+                                          /_/_/_/\` ' `/\_\_\_\
+                                                  )'_'(
+                                             ____.""_"".____
+                                            |___|___|___|___|
+                                              |___|___|___|
+   a single choice can                          |-      |
+   make your life misereble or HELL             |       |
+                        /                      _|_______|_
+                    ___                       |___|___|___|
+                   /   \                        |'   | ||
+                   )'__/                        |       |
+                   \(\\                         |       |
+                     _\\__                      |       |
+                    /,_ \--\                    |     - |
+               ____// \ |   \                   |       |
+              /,-' `  _)/   |\_                 |       |
+              ``     ,o_:)  ```                 |       |
+         .          /    \        .             |       |
+                   /      \                     | |     |
+      .    (      c        c     )    .         |       |
+       (    )      \      /     (    )          |       |
+      ( )  '        |     |      '  ( )        _|_______|_
+      _#, .    ...,:o    o:,..    . ,#_ ,, ,,,|___b'ger___|,,,        
+                    """)
+
+    
+    
+
+
+
+
+
+
